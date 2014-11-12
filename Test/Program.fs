3bdb8ca98c2ee0db0f890af321e40a4d495e8421
Cameron Taggart <cameron@tachyus.com>
2014-11-11 17:39:48 -0800

    SourceLink#35 imported AbstractIL from FCS switched to Paket

--- Test/Program.fs
+++ Test/Program.fs
@@ -1,8 +1,11 @@
-﻿open System
+﻿module Test
+
+open System
 open System.Diagnostics
 open System.Runtime.InteropServices
 open Microsoft.Samples.Tools.Mdbg.Extensions
-open FSharp.Data
+//open FSharp.Data
+open System.Reflection
 
 let symInit() =
     let hProcess = new IntPtr(Random().Next())
@@ -21,10 +24,21 @@ let printModules() =
     for a in ad.GetAssemblies() do
         printfn "%s" a.FullName
 
+let printMethods() =
+    let a = Assembly.LoadFrom @"C:\Projects\scoutplugin\packages\SourceLink\lib\net45\SourceLink.dll"
+    for dt in a.DefinedTypes do
+        printfn "\n%s" dt.FullName
+        for m in dt.GetMembers() do
+            printfn "  %d %s" m.MetadataToken m.Name
+
+        
+
 [<EntryPoint>]
 let main argv =
-    let h = Http.Request("http://www.google.com")
-    printModules()
+//    let h = Http.Request("http://www.google.com")
+//    printModules()
 
 //    printfn "%A" h.StatusCode
+
+    printMethods()
     0
